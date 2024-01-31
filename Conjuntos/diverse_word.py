9fa8408e28c20ca29abb5727e7b2b5baa5c83788
José Manuel Peña Rodríguez <josepenrod@gmail.com>
2024-01-31 10:05:04 +0000

    Solucionados problemas de sincronización despues de numerosas pruebas fallidas

---
+++ Conjuntos/diverse_word.py
@@ -0,0 +1,14 @@
+# ***************
+# PALABRA DIVERSA
+# ***************
+
+
+def run(words: list) -> str:
+    # TU CÓDIGO AQUÍ
+    dword = 'output'
+
+    return dword
+
+
+if __name__ == '__main__':
+    run(['dictionary', 'turtle', 'flexibility', 'humanity'])
